8fcf62395a37f882b175a6f7763bc261b4708956
Joseph <jhur3@u.rochester.edu>
2019-02-28 20:36:41 -0500

    done

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php $title = 'Courses'; ?>
 
-<?php include "inc/html-top.inc"; ?>	
+<?php include "inc/html-top.inc"; ?>
 
 <?php include "inc/nav.inc"; ?>
 
@@ -8,11 +8,23 @@
 
 <section>
 
+	<div class="courses">
+
 	<h2>Courses</h2>
-	
-		<p>
-			Along with CSC 174, this semester I’m taking CSC 191/291 (Machines & Consciousness), CSC 242 (Introduction to Artificial Intelligence), MUR 168A (West African Drumming Introduction), and PSC 288 (Game Theory).  Some of my favorite courses in the past have been CSC 173 (Computation & Formal Systems), PSC 248 (Discrimination), CSC 170 (Introduction to Web Development), CSC 299W (Social Implications of Computing), and all of the courses required for my cluster in American Sign Language.  I wish I could go back in time, and try out some new courses, or audit more courses I'm interested in but that don't fit my degree requirements.  Some of my favorite courses, such as CSC 170 and PSC 248, started off as interests or curiosity that did not contribute to my degree (not my major, minor, or cluster), but the course either eventually became applicable once I realized it was in line with what I wanted to do or pointed me towards more courses in topics I discovered that I enjoyed.
-		</p>
+
+	<p>
+		Along with CSC 174, this semester I’m taking CSC 191/291 (Machines & Consciousness), CSC 242 (Introduction to
+		Artificial Intelligence), MUR 168A (West African Drumming Introduction), and PSC 288 (Game Theory). Some of my
+		favorite courses in the past have been CSC 173 (Computation & Formal Systems), PSC 248 (Discrimination), CSC 170
+		(Introduction to Web Development), CSC 299W (Social Implications of Computing), and all of the courses required
+		for my cluster in American Sign Language. I wish I could go back in time, and try out some new courses, or audit
+		more courses I'm interested in but that don't fit my degree requirements. Some of my favorite courses, such as
+		CSC 170 and PSC 248, started off as interests or curiosity that did not contribute to my degree (not my major,
+		minor, or cluster), but the course either eventually became applicable once I realized it was in line with what
+		I wanted to do or pointed me towards more courses in topics I discovered that I enjoyed.
+	</p>
+
+</div>
 
 </section>
 
